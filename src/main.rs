c473594d41fe0d789a29eb8ccbf847a9138bfb88
user-debian <74452307+MaSTerAK-902@users.noreply.github.com>
2023-04-12 08:53:51 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use algorithm_dictionary::cryptosystem;
+
 mod algorithm_dictionary;
 fn main() {
     let plane_message = "Hello,World!";
-    let secret_key = 12345;
+    let secret_key = 123;
+    let un_key = 12;
 
     //暗号化
+    let encrypted = cryptosystem(plane_message, secret_key);
+    println!("Encrypted: {:?}", encrypted);
 
-    //復号化
-    
+    // 復号化
+    let decrypted = cryptosystem(&String::from_utf8_lossy(&encrypted), un_key);
+    let decrypted_message = String::from_utf8(decrypted).unwrap();
+    println!("Decrypted: {:?}", decrypted_message);
 }
